38a7b0414eafa5f0a887dfff721722b2db222dc7
Gabriel Carneiro <gabriel.carneiro97@live.com>
2018-01-30 09:14:34 -0200

    Resolvendo o problema de vulnerabilidade no electron

--- app/process.js
+++ app/process.js
@@ -1,30 +1,31 @@
-'use strict';
+'use strict'
 
-const pdf = require(__dirname + '/../pdfCreator.js');
-const timer = require('timers');
-const {ipcRenderer} = require('electron');
+const path = require('path')
+const pdf = require(path.join(__dirname, '/../pdfCreator.js'))
+const timer = require('timers')
+const {ipcRenderer} = require('electron')
 
 ipcRenderer.on('message', (e, m) => {
+  if (m.type === 'start') {
+    console.log(m)
 
-  if(m.type === 'start'){
+    let path = m.data
 
-    console.log(m);
+    ipcRenderer.send('toUi', {type: 'start'})
 
-    let path = m.data;
-
-    ipcRenderer.send('toUi', {type: 'start'});
-    
     pdf.readDir(path, status => {
-      let percent = status.now/status.total;
-      ipcRenderer.send('toUi', {type: 'process', data: percent});
+      let percent = status.now / status.total
+
+      ipcRenderer.send('toUi', {type: 'process', data: percent})
     }, info => {
-      if(info === 'ok')
+      if (info === 'ok') {
         timer.setTimeout(() => {
-            ipcRenderer.send('toUi', {type:'end'});
-        }, 1000);
-      if(info === 'null')
-        ipcRenderer.send('toUi', {type: 'null'});
-    });
+          ipcRenderer.send('toUi', { type: 'end' })
+        }, 1000)
+      } else if (info === 'null') {
+        ipcRenderer.send('toUi', { type: 'null' })
+      }
+    })
   }
 
-});
+})
--- main.js
+++ main.js
@@ -1,64 +1,61 @@
-'use strict';
+'use strict'
 
-const {app, BrowserWindow, ipcMain} = require('electron');
-const path = require('path');
-const url = require('url');
+const {app, BrowserWindow, ipcMain} = require('electron')
+const path = require('path')
+const url = require('url')
 
-let win;
-let backgroundWin;
+let win
+let backgroundWin
 
 var createWindow = () => {
-
   win = new BrowserWindow({
     width: 300,
     height: 250,
     resizable: false,
-    backgroundColor: "#e0e0e0",
+    backgroundColor: '#e0e0e0',
     autoHideMenuBar: true
-  });
+  })
 
-  backgroundWin = new BrowserWindow({show: false});
+  backgroundWin = new BrowserWindow({show: false})
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'app/index.html'),
     protocol: 'file:',
     slashes: true
-  }));
+  }))
 
   backgroundWin.loadURL(url.format({
     pathname: path.join(__dirname, 'app/process.html'),
     protocol: 'file:',
     slashes: true
-  }));
+  }))
 
   // win.webContents.openDevTools({detach: true});
 
   // backgroundWin.webContents.openDevTools({detach: true});
 
   win.on('closed', () => {
-    win = null;
-    backgroundWin.destroy();
-  });
+    win = null
+    backgroundWin.destroy()
+  })
 
   backgroundWin.on('closed', () => {
-    backgroundWin = null;
-  });
+    backgroundWin = null
+  })
 
   ipcMain.on('toUi', (e, m) => {
-    win.webContents.send('message', m);
-  });
+    win.webContents.send('message', m)
+  })
 
   ipcMain.on('toProcessor', (e, m) => {
-    backgroundWin.webContents.send('message', m);
-  });
-
+    backgroundWin.webContents.send('message', m)
+  })
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
